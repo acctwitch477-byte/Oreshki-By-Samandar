d1f8f17030974774d158cdb9d9db0be9a3a6626b
acctwitch477-byte <acctwitch477@gmail.com>
2025-08-07 16:28:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Простая логика заглушки
+document.getElementById("app").innerHTML = `
+  <button onclick="alert('Забрать заказ')">Забрать заказ</button>
+  <button onclick="alert('Заказы')">Заказы</button>
+  <button onclick="alert('Калькулятор')">Калькулятор</button>
+  <button onclick="alert('Архив')">Архив</button>
+`;
